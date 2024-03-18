3e91590fe213747d010ba1c07fd9bf3695d3cb02
Gyamfi John <115163376+gyamficodes@users.noreply.github.com>
2024-03-18 00:56:39 -0700

    udate

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
+/* eslint-disable jsx-a11y/alt-text */
 import { Inter } from "next/font/google";
 import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
+
 
 export const metadata = {
   title: "Users Management App",
@@ -10,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+     
       <body className={inter.className}>{children}</body>
     </html>
   );
